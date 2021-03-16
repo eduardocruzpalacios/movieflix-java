c6a8fb6b37d98d09f124a6d4d748188cbb51e087
educp <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-03-16 15:25:30 +0100

    gui menú principal y menú gestión de usuarios

---
+++ src/gui/Menu.java
@@ -0,0 +1,24 @@
+package gui;
+
+public class Menu {
+
+	public static void principal() {
+		System.out.println("\n********** MENÚ PRINCIPAL **********\n");
+		System.out.println("¿Qué quieres hacer?");
+		System.out.println("1. Gestionar Usuarios");
+		System.out.println("2. Gestionar Películas");
+		System.out.println("3. Salir del programa");
+	}
+	
+	public static void usuarios() {
+		System.out.println("\n********** GESTIÓN DE USUARIOS **********\n");
+		System.out.println("¿Qué quieres hacer?");
+		System.out.println("1. Listar todos los usuarios");
+		System.out.println("2. Añadir un usuario");
+		System.out.println("3. Eliminar un usuario");
+		System.out.println("4. Modificar un usuario");
+		System.out.println("5. Volver al menú principal");
+		System.out.println("6. Salir del programa");
+	}
+	
+}
