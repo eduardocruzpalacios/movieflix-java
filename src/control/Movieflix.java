2068cff32fabbf14e6f3adb195f2dd87120b3bd4
unknown <80522547+Arrshe@users.noreply.github.com>
2021-03-17 13:53:03 +0100

    He creado los métodos de la clase Movieflix de la capa controller

---
+++ src/control/Movieflix.java
@@ -0,0 +1,80 @@
+package control;
+
+import gui.Menu;
+import servicios.Gestor;
+import servicios.GestorUsuarios;
+import utilidades.Lector;
+
+public class Movieflix {
+	private Gestor gestor = new GestorUsuarios();
+
+	public void abrirMovieflix() {
+		boolean seguir = true;
+		int opcion = 0;
+		do {
+			Menu.principal();
+			opcion = Lector.pedirIntEntre(1, 3);
+			if (opcion == 3) {
+				seguir = false;
+			} else {
+				seleccionarOpciones(opcion);
+			}
+		} while (seguir);
+		System.out.println(" --- Fin de la sesion ---");
+	}
+
+	public void seleccionarOpciones(int opcion) {
+
+		switch (opcion) {
+
+		case 1:
+			// Gestionar usuarios
+
+			boolean seguir = true;
+			int opcionUser = 0;
+			do {
+				gui.Menu.usuarios();
+				opcionUser = Lector.pedirIntEntre(1, 5);
+				if (opcionUser == 5) {
+					seguir = false;
+				} else {
+					gestionarUsuarios(opcionUser);
+				}
+			} while (seguir);
+
+			// case 2: //Hay que hacer la parte de películas
+			// Gestionar películas
+			// gui.Menu.
+
+		}
+
+	}
+
+	// Switch para elegir opciones dentro de la gestión de usuarios
+	public void gestionarUsuarios(int opcionUser) {
+
+		switch (opcionUser) {
+
+		case 1:
+			// Listar todos los usuarios
+
+			gestor.listarUsuarios();
+
+		case 2:
+			// Añadir usuario
+
+			gestor.addUsuarios();
+
+		case 3:
+			// Eliminar usuario
+			gestor.eliminarUsuarios();
+
+		case 4:
+			// Modificar usuario
+
+			gestor.modificarUsuarios();
+
+		}
+
+	}
+}
