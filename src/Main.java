ab8e83a1cf7f1a6db07d5b14bcb462f8055e59b7
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-08-13 12:37:39 +0200

    move execution class to the project root

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Fecha: 29/10/2021
+ *
+ * @author Edu
+ *
+ * @version: 2.0
+ */
+import controller.Movieflix;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Movieflix().ejecutar();
+	}
+
+}
